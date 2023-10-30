0a15274d2522fb840b8deade074810976a73e8b6
jnccd <kobidogao@outlook.com>
2023-10-30 18:50:03 +0100

    Init ship obj

---
+++ socha/ship.py
@@ -0,0 +1,12 @@
+from socha.field import CubeCoords, Dir, Team
+
+class Ship:
+    def __init__(self, coal: int, dir: Dir, free_turns: int, passengers: int, points: int, speed: int, team: Team, pos: CubeCoords):
+        self.coal = coal
+        self.dir = dir
+        self.free_turns = free_turns
+        self.passengers = passengers
+        self.points = points
+        self.speed = speed
+        self.team = team
+        self.pos = pos
