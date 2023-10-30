25082c64f2f2cb82a4372a8ead45d88be49e448b
Niklas Carstensen <kobidogao@yahoo.com>
2023-10-30 13:18:22 +0100

    Do a bunch of stuff

--- socha/Starter.py
+++ socha/Starter.py
@@ -1,5 +1,39 @@
-
+import socket
+from lxml import objectify
 
 class Starter:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, calculate_move, on_update) -> None:
+        self.calculate_move = calculate_move
+        self.on_update = on_update
+        
+        self.do_a_barrel_roll()
+        
+    def do_a_barrel_roll(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.1)
+        sock.connect(("localhost", 13050))
+        
+        # Welcome
+        sock.send(f"<protocol><join gameType=\"swc_2023_penguins\" />".encode())
+        
+        while True: # Rolls here
+            # Receive answer and coat it
+            answer: str = self.receive(sock)
+            answer = answer.removeprefix('<protocol>')
+            answer = "<received>" + answer + "</received>"
+            if answer.__contains__('</protocol>'):
+                break
+            
+            parsed_answer = objectify.fromstring(answer)
+            print(answer, parsed_answer)
+            
+            
+            
+        s.close()
+        
+    def receive(self, sock) -> str:
+        received: str = ''
+        while not received.__contains__('</room>') and not received.__contains__('</protocol>'):
+            received += sock.recv(256).decode()
+        #print(received)
+        return received
--- socha/__init__.py
+++ socha/__init__.py
@@ -1,3 +1,16 @@
-import Move
-import GameState
-import Starter
+import socha.game_state as game_state
+import socha.move as move
+import socha.starter as starter
+
+Move = move.Move
+GameState = game_state.GameState
+Starter = starter.Starter
+
+__all__ = [
+    'game_state',
+    'move',
+    'starter',
+    'Move',
+    'GameState',
+    'Starter',
+]
